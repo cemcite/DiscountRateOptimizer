0137102633838862dc3245a23a0fe2739430ea8f
cemcite <cemcite@gmail.com>
2021-08-24 14:07:24 +0300

    Add files via upload

---
+++ DiscontRateOptimizer.sql
@@ -0,0 +1,15 @@
+﻿BEGIN TRANSACTION;
+/* Create a table called SPECIALOFFERS */
+CREATE TABLE SPECIALOFFERS(Id integer PRIMARY KEY, RoomPrice decimal, DiscountRate decimal, Category int);
+/* Create few records in this table */
+INSERT INTO SPECIALOFFERS VALUES(1, 1000, 2, 1);
+INSERT INTO SPECIALOFFERS VALUES(2, 2000, 2, 2);
+INSERT INTO SPECIALOFFERS VALUES(3, 1500, 3, 1);
+INSERT INTO SPECIALOFFERS VALUES(4, 750, 5, 2);
+INSERT INTO SPECIALOFFERS VALUES(5, 1000, 10, 5);
+COMMIT;
+
+-- Select category by the max discount rate. 
+SELECT Category, SUM(DiscountRate*(RoomPrice/100)) AS DiscountRate FROM SPECIALOFFERS GROUP BY Category ORDER BY  DiscountRate DESC;
+
+SELECT * FROM SPECIALOFFERS WHERE Category = (SELECT Category FROM (SELECT Category, MAX(DiscountRate) FROM (SELECT Category, SUM(DiscountRate) AS DiscountRate FROM SPECIALOFFERS GROUP BY Category ORDER BY  DiscountRate DESC)));
---
+++ DiscontRateOptimizer_v3.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // SETUP
+            List<SpecialOffer> specialOfferList = Setup();
+            // SETUP
+
+            // CALCULATION
+            List<OptimizedSpecialOffer> optimizedSpecialOfferList = new List<OptimizedSpecialOffer>();
+            for (int i = 0; i < specialOfferList.Count; i++)
+            {
+                decimal totalDiscountRate = specialOfferList.Where(specialOffer => specialOfferList[i].CombinableWith.Contains(specialOffer.ID)).Select(specialOffer => specialOffer.DiscountRate).Sum() + specialOfferList[i].DiscountRate;
+                optimizedSpecialOfferList.Add(new OptimizedSpecialOffer
+                {
+                    SpecialOfferList = string.Join(",", new int[] { specialOfferList[i].ID }.Concat(specialOfferList[i].CombinableWith)),
+                    TotalDiscountRate = totalDiscountRate
+
+                });
+            }
+            // CALCULATION
+
+            // OUTPUT
+            Console.WriteLine("Discount ID List \tTotal Discount Rate");
+            foreach (OptimizedSpecialOffer specialOffer in optimizedSpecialOfferList.OrderByDescending(specialOffer => specialOffer.TotalDiscountRate))
+            {
+                Console.WriteLine(string.Concat(specialOffer.SpecialOfferList, "\t\t\t\t\t", specialOffer.TotalDiscountRate));
+            }
+            // OUTPUT
+        }
+
+
+        /// <summary>
+        /// Generates example data
+        /// </summary>
+        /// <returns></returns>
+        public static List<SpecialOffer> Setup()
+        {
+            return new List<SpecialOffer>()
+            {
+                new SpecialOffer
+                {
+                    ID = 1,
+                    DiscountRate = 2,
+                    CombinableWith = new int[] { 2,3 }
+                },
+                new SpecialOffer
+                {
+                    ID = 2,
+                    DiscountRate = 4,
+                    CombinableWith = new int[] {}
+                },
+                new SpecialOffer
+                {
+                    ID = 3,
+                    DiscountRate = 2,
+                    CombinableWith = new int[] {}
+                },
+                new SpecialOffer
+                {
+                    ID = 4,
+                    DiscountRate = 5,
+                    CombinableWith = new int[] {5}
+                },
+                new SpecialOffer
+                {
+                    ID = 5,
+                    DiscountRate = 10,
+                    CombinableWith = new int[] {}
+                },
+                new SpecialOffer
+                {
+                    ID = 6,
+                    DiscountRate = 15,
+                    CombinableWith = new int[] {}
+                },
+                new SpecialOffer
+                {
+                    ID = 7,
+                    DiscountRate = 9,
+                    CombinableWith = new int[] {}
+                },
+                new SpecialOffer
+                {
+                    ID = 8,
+                    DiscountRate = 1,
+                    CombinableWith = new int[] {1}
+                }
+            };
+        }
+    }
+
+    /// <summary>
+    /// SpecialOffer
+    /// </summary>
+    public class SpecialOffer
+    {
+        /// <summary>
+        /// Gets from service
+        /// </summary>
+        public int ID;
+        /// <summary>
+        /// Gets from service
+        /// </summary>
+        public decimal DiscountRate;
+        /// <summary>
+        /// Sets as a result of the calculation
+        /// </summary>
+        public decimal TotalDiscountAmount;
+        /// <summary>
+        /// Other offers which combinable
+        /// </summary>
+        public int[] CombinableWith;
+    }
+
+    public class OptimizedSpecialOffer
+    {
+        /// <summary>
+        /// All combined offers
+        /// </summary>
+        public string SpecialOfferList;
+        /// <summary>
+        /// Combined offers' total discount rate
+        /// </summary>
+        public decimal TotalDiscountRate;
+    }
+}
+
